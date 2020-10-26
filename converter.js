21818af6a648d90b873069fbf5556764e1dee259
juggyhead <73391083+juggyhead@users.noreply.github.com>
2020-10-26 15:21:03 +0530

    Update converter.js

--- converter.js
+++ converter.js
@@ -3,48 +3,40 @@ language = {};
 languagemappings = {};
 
 om = {};  // language independent.
-om.ProcessKeypressEvent = function(e, field) {
+om.translit = function(prev, ch, later) {
+
   console.log("current memory: ", memory);
-  var haskey = 'key' in e;
-  var ch = haskey ? e.key : '';
-  var control_pressed = e['ctrlKey'] || e['metaKey'];
-  if ((ch.length != 1) || control_pressed) {
-    return true;
-  }
-  if (e.keyCode == 13) {  // ENTER
+  // remember this input; cleared later if not needed
+  memory.setMemoryAndTimer(ch);
+
+  if (ch == "\n") {  // ENTER
     memory.clearMemoryAndTimer();
-    return true;
+    return [prev + ch, later];
   }
 
   var MapClear = om.getMappingFromEnglish(ch);
 
   if (!MapClear) {
     // no mapping for this char. display it as is.
-    // also add it to memory.
-    memory.setMemoryAndTimer(ch, strbeforecursor);
-    return true;
+    return [prev + ch, later];
   }
 
   MapClear.inputchar = ch;
 
-  var strbeforecursor =
-      om.insertMappedString(MapClear, field);
+  var strbeforecursor = om.insertMappedString(MapClear, prev);
 
-  if (!!strbeforecursor) {
-    e.preventDefault();
-    // to prevent a bug when typing "jai" converts to "ऐ", we do not store in
-    // memory when we remove virama ('a' after 'j')
-    if (MapClear.backspaceonlyvirama) {
-      memory.setMemoryAndTimer(ch, strbeforecursor);
-    } else {
-      memory.setMemoryAndTimer(ch, strbeforecursor);
-    }
-  } else {
+  if (!strbeforecursor) {
     // no chars added. maybe a backspace. or some neutralizer.
     memory.clearMemoryAndTimer();
   }
+  return [strbeforecursor, later];
 }
 
+// 1. gets mapping of input char + memory
+// 2. determines if backspace virama, matra or full char
+// sV + h = shV
+// ja + i = jAI
+// ji + i = jAI
 om.getMappingFromEnglish = function(inputch) {
   if (!inputch) {
     return null;
@@ -96,95 +88,74 @@ om.getMappingFromEnglish = function(inputch) {
   return null;
 }
 
-om.insertMappedString = function(MapClear, field) {
+om.insertMappedString = function(MapClear, prev) {
   var mappedstr = MapClear.str;
   var inputchar = MapClear.inputchar;
 
-  var NVO = utils.getNodeValueOffsetAtCursor(field);
-  if (!NVO) {
-    return false;
-  }
-  var node = NVO.node;
-  var oldvalue = NVO.value;
-  var offset = NVO.offset;
   var newstringbeforecursor = mappedstr;
-  var newvalue = newstringbeforecursor;
-  if (!!oldvalue) {
-    // slice in str into oldvalue.
-    var oldstringbeforecursor = oldvalue.substr(0, offset);
-    if (oldstringbeforecursor.length > 0) {
-      // for cases like sV + h to make it shV (replace
-      // sV with shV.)
-      if (MapClear.backspace) {
-        oldstringbeforecursor = om.backspaceAndRemoveVirama(oldstringbeforecursor);
-      } else if (MapClear.backspaceonlyvirama) {
-        var oldlen = oldstringbeforecursor.length;
-        oldstringbeforecursor = om.backspaceOnlyVirama(oldstringbeforecursor);
-        var newlen = oldstringbeforecursor.length;
-        if (newlen == oldlen-1) {
-          // virama was removed.
-          // if the input was 'a', mission accomplished.
-          // nothing more to append.
-          // Note: we used mappedstr as inputchar is not available.
-          //if (mappedstr == language.a) {
-          if (inputchar == 'a') {
-            mappedstr = '';
-          }
+  if (!prev) {
+    return newstringbeforecursor;
+  }
+  var oldstringbeforecursor = prev;
+  if (oldstringbeforecursor.length > 0) {
+    // for cases like sV + h to make it shV (replace
+    // sV with shV.)
+    if (MapClear.backspace) {
+      oldstringbeforecursor = om.backspaceAndRemoveVirama(oldstringbeforecursor);
+    } else if (MapClear.backspaceonlyvirama) {
+      var oldlen = oldstringbeforecursor.length;
+      oldstringbeforecursor = om.backspaceOnlyVirama(oldstringbeforecursor);
+      var newlen = oldstringbeforecursor.length;
+      if (newlen == oldlen-1) {
+        // virama was removed.
+        // if the input was 'a', mission accomplished.
+        // nothing more to append.
+        // Note: we used mappedstr as inputchar is not available.
+        //if (mappedstr == language.a) {
+        if (inputchar == 'a') {
+          mappedstr = '';
         }
       }
     }
-
-    // If at the start of a line or word, convert matras
-    // to their corresponding vowel forms.
-    // Typical sequence of events that happen when one types
-    // "o" followed by "o" as a new word:
-    // 1) the first "o" is converted to matra.o above, which is then
-    //  converted to vowel.o here.
-    // 2) the second "o" is attached to the first and mapped to
-    //  matra.oo, which triggers a backspace. This backspace removes
-    //  vowel.o and makes it matra.oo above. The below code then
-    //  converts matra.oo to vowel.oo.
-    // 3) only for the input char 'a', this sequence is disrupted
-    //  because of the above code to handle removal of virama.
-    // 4) Hence only for letter 'a', we allow an exception.
-    var isWordStart = !oldstringbeforecursor ||
-      (oldstringbeforecursor.charAt(
-        oldstringbeforecursor.length-1) == ' ') ||
-      (oldstringbeforecursor.charAt(
-        oldstringbeforecursor.length-1).charCodeAt(0) == 160);
-    if (isWordStart) {
-        // Convert matras to vowels.
-        if (mappedstr in language.matra_to_vowel) {
-          mappedstr = language.matra_to_vowel[mappedstr];
-        }
-    }
-    // Exception handling for case when we have language.A before
-    // cursor and the input is 'a'.
-    if (!!oldstringbeforecursor &&
-        (oldstringbeforecursor.charAt(
-          oldstringbeforecursor.length-1) == language.A) &&
-        (inputchar == 'a')) {
-      // remove language.A and insert language.AA
-      oldstringbeforecursor = om.backspaceAndRemoveVirama(oldstringbeforecursor);
-      mappedstr = language.AA;
-    }
-
-    newstringbeforecursor = oldstringbeforecursor + mappedstr;
-    newvalue = newstringbeforecursor + oldvalue.substr(offset);
   }
 
-  // update the field.
-  if (utils.IsTextNode(node)) {
-    node.textContent = newvalue;
-  } else {
-    node.innerHTML = newvalue;
+  // If at the start of a line or word, convert matras
+  // to their corresponding vowel forms.
+  // Typical sequence of events that happen when one types
+  // "o" followed by "o" as a new word:
+  // 1) the first "o" is converted to matra.o above, which is then
+  //  converted to vowel.o here.
+  // 2) the second "o" is attached to the first and mapped to
+  //  matra.oo, which triggers a backspace. This backspace removes
+  //  vowel.o and makes it matra.oo above. The below code then
+  //  converts matra.oo to vowel.oo.
+  // 3) only for the input char 'a', this sequence is disrupted
+  //  because of the above code to handle removal of virama.
+  // 4) Hence only for letter 'a', we allow an exception.
+  var isWordStart = !oldstringbeforecursor ||
+    (oldstringbeforecursor.charAt(
+      oldstringbeforecursor.length-1) == ' ') ||
+    (oldstringbeforecursor.charAt(
+      oldstringbeforecursor.length-1).charCodeAt(0) == 160);
+  if (isWordStart) {
+      // Convert matras to vowels.
+      if (mappedstr in language.matra_to_vowel) {
+        mappedstr = language.matra_to_vowel[mappedstr];
+      }
+  }
+  // Exception handling for case when we have language.A before
+  // cursor and the input is 'a'.
+  if (!!oldstringbeforecursor &&
+      (oldstringbeforecursor.charAt(
+        oldstringbeforecursor.length-1) == language.A) &&
+      (inputchar == 'a')) {
+    // remove language.A and insert language.AA
+    oldstringbeforecursor = om.backspaceAndRemoveVirama(oldstringbeforecursor);
+    mappedstr = language.AA;
   }
 
-  // to handle cases where sometimes the caret goes missing.
-  utils.placeCursorAfterStringInNode(window, node, newstringbeforecursor);
-
-  var ret = om.getSuffix(newstringbeforecursor);
-  return ret;
+  newstringbeforecursor = oldstringbeforecursor + mappedstr;
+  return newstringbeforecursor;
 }
 
 
